3262a8dc6cc23f61009d1cbd782dcf212e91a8fd
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-11-27 10:11:58 +0500

    mvp with drag & zoom delay

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header/Header";
 import GraphEditor from "@/components/GraphEditor/GraphEditor";
+import StepManager from "@/components/StepManager/StepManager";
 export default function Home() {
   return (
     <>
       <Header />
       <GraphEditor />
+      <StepManager />
     </>
   );
 }
